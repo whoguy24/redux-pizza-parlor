0ad19d8f700bb50b2619822ea2ae002db9c40744
Yasir Uddin <yasir.uddin@icloud.com>
2021-12-02 13:40:06 -0700

    created initial pseudocode for pizzaOrders reducer on index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import redux items: createStore, combineReducers, applyMiddleware
+
+// import Provider from react-redux
+
+// import logger from redux logger
+
+
 import './index.css';
 import App from './components/App/App';
 
+
+
+// We have to make our pizzaOrders Reducer
+
+// We have to create our Redux Store
+
+// We have to make sure this is sandwiched by Provider
 ReactDOM.render(<App />, document.getElementById('root'));
