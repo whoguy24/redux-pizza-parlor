4497c342040465f3d4a7ea0ec3341767e4f6cd72
Yasir Uddin <yasir.uddin@icloud.com>
2021-12-02 13:01:59 -0700

    laid out basic structure of order GET route

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,17 +3,23 @@ import React from 'react';
 // Make sure axios is imported.
 import axios from 'axios';
 // Make sure useEffect is imported
+import {useEffect} from 'react';
 // Make sure useDispatch is imported
+import {useDispatch} from 'react-redux';
 import './App.css';
 
 function App() {
 
 // Make sure to include useDispatch functionality here
+  const dispatch = useDispatch();
 
 // Need useEffect to render our GET request:
   // useEffect arrow function
       // inside is the axios GET request function
       // before ending function, include an empty array
+  useEffect(() => {
+    makePizzaOrdersReducerMatchTheStateOfOurPizzaOrdersTable();
+  }, []);
 
 // Need to make the axios GET request function
   // method - GET
@@ -24,6 +30,19 @@ function App() {
         //  payload: res.data aka response data recieved
       // end dispatch
 // end the axios GET request function
+makePizzaOrdersReducerMatchTheStateOfOurPizzaOrdersTable = () => {
+    axios({
+      method: 'GET',
+      url: '/api/order'
+    })
+    .then((res) => {
+      const pizzaOrders = res.data;
+      dispatch({
+        type: 'SET_PIZZA_ORDERS',
+        payload: pizzaOrders
+      })
+    })
+  };
 
   return (
     <div className='App'>
@@ -34,7 +53,7 @@ function App() {
       <img src='images/pizza_photo.png' />
       <p>Pizza is great.</p>
       {/* Insert a tag, insert Order Component name, insert name of axios GET request function as a prop */}
-
+      <PizzaOrders makePizzaOrdersReducerMatchTheStateOfOurPizzaOrdersTable={makePizzaOrdersReducerMatchTheStateOfOurPizzaOrdersTable} />
     </div>
   );
 }
