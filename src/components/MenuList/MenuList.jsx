cd5c7a6fb6d38b79dd40238a2984b13656c43bc8
whoguy24 <whoguy24@gmail.com>
2021-12-03 11:26:59 -0600

    Built cart functions.

--- src/components/MenuList/MenuList.jsx
+++ src/components/MenuList/MenuList.jsx
@@ -4,6 +4,21 @@ import { useSelector } from 'react-redux';
 function MenuList() {
 
   const pizzas = useSelector((store) => store.menuReducer);
+  const cart = useSelector((store) => store.cartReducer);
+
+  function addPizzaToCart(pizzaObject) {
+    dispatch({
+      type: 'ADD_PIZZA_TO_CART',
+      payload: pizzaObject
+    })
+  }
+
+  function removePizzaFromCart (pizzaObject) {
+    dispatch({
+      type: 'REMOVE_PIZZA_FROM_CART',
+      payload: pizzaObject
+    })
+  }
 
   return (
     <div>
