4c08483c2a711fb63347403fca341145ced6d4c9
Yasir Uddin <yasir.uddin@icloud.com>
2021-12-03 10:29:37 -0700

    initial pseudocode to display pizzas

--- src/components/MenuList/MenuList.jsx
+++ src/components/MenuList/MenuList.jsx
@@ -5,6 +5,12 @@ function MenuList() {
 
   const pizzas = useSelector((store) => store.menuReducer);
 
+// We need to import the pizza table from Postico and return the pizzas to display:
+  // We have to loop through the items. Instead of returning yay a pizza lives here, how can we return the pizzas? 🤔
+// We need to use Warren's pizza GET ROUTE from App.jsx
+// Maybe we need to use the menuReducer from index.js too
+// Can't think of anything else we need to do at the moment. 
+
   return (
     <div>
       <ul>
